c9cfe3d128825c388263a69cb9be2f1b30e6dc24
Pikachuun <pikachuun@gmail.com>
2013-04-21 17:09:53 -0300

    Status/Weather Modifications

---
+++ mods/pxy/statuses.js
@@ -0,0 +1,183 @@
+function clampIntRange(num, min, max) {
+  num = Math.floor(num);
+	if (num < min) num = min;
+	if (typeof max !== 'undefined' && num > max) num = max;
+	return num;
+}
+exports.BattleStatuses = {
+	frz: {
+		effectType: 'Status',
+		onStart: function(target) {
+			this.add('-status', target, 'frz');
+		},
+		onBeforeMovePriority: 2,
+		onBeforeMove: function(pokemon, target, move) {
+			if (move.thawsUser || this.random(2) === 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'frz');
+			return false;
+		},
+		onHit: function(target, source, move) {
+			if (move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		}
+	},
+	raindance: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function(source, effect) {
+			if (source && source.item === 'damprock') {
+				return 8;
+			},
+			else if (source && source.ability === 'forecast') {
+				return 0;
+			}
+			return 5;
+		},
+		onBasePower: function(basePower, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('rain water boost');
+				return basePower * 1.5;
+			}
+			if (move.type === 'Fire') {
+				this.debug('rain fire suppress');
+				return basePower * .5;
+			}
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				this.effectData.duration = 0;
+				this.add('-weather', 'RainDance', '[from] ability: '+effect, '[of] '+source);
+			} else {
+				this.add('-weather', 'RainDance');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'RainDance', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onEnd: function() {
+			this.add('-weather', 'none');
+		}
+	},
+	sunnyday: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function(source, effect) {
+			if (source && source.item === 'heatrock') {
+				return 8;
+			},
+			else if (source && source.ability === 'forecast') {
+				return 0;
+			}
+			return 5;
+		},
+		onBasePower: function(basePower, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Sunny Day fire boost');
+				return basePower * 1.5;
+			}
+			if (move.type === 'Water') {
+				this.debug('Sunny Day water suppress');
+				return basePower * .5;
+			}
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				this.effectData.duration = 0;
+				this.add('-weather', 'SunnyDay', '[from] ability: '+effect, '[of] '+source);
+			} else {
+				this.add('-weather', 'SunnyDay');
+			}
+		},
+		onImmunity: function(type) {
+			if (type === 'frz') return false;
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'SunnyDay', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onEnd: function() {
+			this.add('-weather', 'none');
+		}
+	},
+	sandstorm: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function(source, effect) {
+			if (source && source.item === 'smoothrock') {
+				return 8;
+			},
+			else if (source && source.ability === 'forecast') {
+				return 0;
+			}
+			return 5;
+		},
+		onModifySpD: function(spd, pokemon) {
+			if (pokemon.hasType('Rock')) {
+				return spd * 3/2;
+			}
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				this.effectData.duration = 0;
+				this.add('-weather', 'Sandstorm', '[from] ability: '+effect, '[of] '+source);
+			} else {
+				this.add('-weather', 'Sandstorm');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'Sandstorm', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onWeather: function(target) {
+			this.damage(target.maxhp/16);
+		},
+		onEnd: function() {
+			this.add('-weather', 'none');
+		}
+	},
+	hail: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function(source, effect) {
+			if (source && source.item === 'icyrock') {
+				return 8;
+			},
+			else if (source && source.ability === 'forecast') {
+				return 0;
+			}
+			return 5;
+		},
+		onModifyDef: function(def, pokemon) {
+			if (pokemon.hasType('Ice')) {
+				return def * 3/2;
+			}
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				this.effectData.duration = 0;
+				this.add('-weather', 'Hail', '[from] ability: '+effect, '[of] '+source);
+			} else {
+				this.add('-weather', 'Hail');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'Hail', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onWeather: function(target) {
+			this.damage(target.maxhp/16);
+		},
+		onEnd: function() {
+			this.add('-weather', 'none');
+		}
+	}
+};
